eb506ed6c070cc2a78060355ff23ddd9de89c49d
Araali Farooq <farooqp35h@gmail.com>
2014-03-22 18:18:36 +0300

    displaying tasks on the index page

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,18 +1,19 @@
 package controllers;
 
 import play.*;
+import play.api.mvc.Rendering;
+import play.data.Form;
 import play.mvc.*;
 import models.*;
 
-import views.html.*;
-
 public class Application extends Controller {
+
 
     public static Result index() {
         return ok("hello world.");
     }
     public static Result tasks() {
-        return TODO;
+        return ok(views.html.index.render(TaskManager.listOfTasks()));
     }
 
     public static Result newTask() {
